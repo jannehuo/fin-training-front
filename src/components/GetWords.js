c5d9bd8462f59d517583018aaec809ebf57ce01a
jannehuo-kesko <janne.huotari@teamit.fi>
2023-04-08 12:33:36 +0300

    Simple ui for words

---
+++ src/components/GetWords.js
@@ -0,0 +1,64 @@
+import { useState } from 'preact/hooks';
+
+const GetWords = () => {
+  const [amount, setAmount] = useState('');
+  const [words, setWords] = useState([]);
+
+  const handleAmountInput = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const handleButtonClick = async () => {
+    const url = `http://localhost:8080/words/${amount}`;
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    const mappedData = jsonData.map((word) => {
+      return {
+        word,
+        translation: '',
+      };
+    });
+    setWords(mappedData);
+  };
+
+  const checkTranslation = async (word) => {
+    const url = `http://localhost:8080/translate/${word}`;
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    const updatedWordList = words.map((w) => {
+      if (w.word === word) {
+        return {
+          word: w.word,
+          translation: jsonData.result,
+        };
+      }
+      return {
+        ...w,
+      };
+    });
+    setWords(updatedWordList);
+  };
+
+  return (
+    <>
+      <label htmlFor="amount">How Many words?</label>
+      <input
+        id="amount"
+        type="text"
+        value={amount}
+        onChange={handleAmountInput}
+      />
+      <button onClick={handleButtonClick}>Get Words!</button>
+      <div>
+        {words.map((word) => (
+          <p key={word.word}>
+            {word.word} {word.translation}
+            <button onClick={() => checkTranslation(word.word)}>Check!</button>
+          </p>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default GetWords;
---
+++ src/components/app.js
@@ -0,0 +1,9 @@
+import GetWords from './GetWords';
+
+const App = () => (
+  <div id="app">
+    <GetWords />
+  </div>
+);
+
+export default App;
